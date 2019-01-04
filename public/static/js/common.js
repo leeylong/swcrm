06b58659d2dc60631ed19166233f7dceea3e15b4
leeylong <creativeboyme@outlook.com>
2019-01-04 17:42:24 +0800

    更改了表格宽度

--- public/static/js/common.js
+++ public/static/js/common.js
@@ -20,3 +20,30 @@ $("#dayu-button-submit").click(function () {
     },"JSON");
 
 });
+
+
+//公海私海切换
+$(".handlePosi").click(function () {
+        postData = {};
+    
+        //1、获取标签中的数据。
+        var url = SCOPE.update_url;
+        var jump_url = window.location;
+    
+        //2、封装数据
+        postData['id'] = $(this).attr('attr-id');
+        postData['status'] = $(this).attr('attr-status');
+    
+        console.log(postData);
+        //3、发送数据
+        $.post(url,postData,function (result) {
+            console.log(result);
+        //4、返回结果处理
+            if(result.status == 1){
+                return dialog.success(result.message,jump_url);
+            }else{
+                return dialog.error(result.message);
+            }
+        },"JSON");
+    
+    });
